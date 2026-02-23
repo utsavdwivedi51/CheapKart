30f1125eb7215d39ec61ec6a56ccdf79ab2d6710
Utsav Dwivedi <utsavdwivedi51@gmail.com>
2026-02-23 22:54:45 +0530

    Add config.js with configuration constants and utility functions

---
+++ config.js
@@ -0,0 +1,14 @@
+// Configuration constants
+const API_URL = 'https://api.example.com';
+const TIMEOUT = 5000;
+
+// Utility functions
+function getApiUrl() {
+    return API_URL;
+}
+
+function getTimeout() {
+    return TIMEOUT;
+}
+
+module.exports = { getApiUrl, getTimeout };

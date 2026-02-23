07cab903038570579da9c7b5a9b24f94c028c3a7
Utsav Dwivedi <utsavdwivedi51@gmail.com>
2026-02-23 23:01:46 +0530

    Create auth.js with authentication functions

---
+++ auth.js
@@ -0,0 +1,33 @@
+// auth.js
+
+// Function to log in a user
+function login(username, password) {
+    // logic to log in user
+}
+
+// Function to log out a user
+function logout() {
+    // logic to log out user
+}
+
+// Function to sign up a new user
+function signup(username, password) {
+    // logic to sign up user
+}
+
+// Function to get the current logged-in user
+function getCurrentUser() {
+    // logic to get current user
+}
+
+// Function to check if a user is logged in
+function isUserLoggedIn() {
+    // logic to check if user is logged in
+}
+
+// Function to validate user credentials
+function validateCredentials(username, password) {
+    // logic to validate credentials
+}
+
+module.exports = { login, logout, signup, getCurrentUser, isUserLoggedIn, validateCredentials };
